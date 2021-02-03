a0e52b371e1370e30dd91c7e7bd6e3351854837f
Evgeny Kolotov <e.Kolotov1995@gmail.com>
2021-02-03 21:58:54 +0300

    Added a picker with emoji

--- src/components/ChatInput/ChatInput.js
+++ src/components/ChatInput/ChatInput.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import { Button, Input } from 'antd';
 import { UploadField } from '@navjobs/upload';
+import { Picker } from 'emoji-mart';
 
 import './ChatInput.css';
 import { SmileOutlined, CameraOutlined, AudioOutlined, SendOutlined } from '@ant-design/icons';
 
 const ChatInput = () => {
   const [value, setValue] = useState('');
+  const [emojiPickerVisible, setEmojiPickerVisible] = useState(false);
+
+  const toggleEmojiPicker = () => {
+    setEmojiPickerVisible(!emojiPickerVisible);
+  };
 
   return (
     <div className="chat-input">
       <div className="chat-input__smile-btn">
-        <Button type="ghost" shape="circle" icon={<SmileOutlined />} />
+        {emojiPickerVisible && (
+          <div className="chat-input__emoji-picker">
+            <Picker set="apple" />
+          </div>
+        )}
+        <Button onClick={toggleEmojiPicker} type="ghost" shape="circle" icon={<SmileOutlined />} />
       </div>
       <Input
         onChange={(event) => setValue(event.target.value)}
